dc9d75c40e8c9f2e483930d90c603bc42b35abeb
nkambika <nkambika00@gmail.com>
2024-11-29 11:00:39 +0530

    first-commit

---
+++ src/main/java/com/crud/test/CrudOperationTestApplication.java
@@ -0,0 +1,13 @@
+package com.crud.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationTestApplication.class, args);
+	}
+
+}
